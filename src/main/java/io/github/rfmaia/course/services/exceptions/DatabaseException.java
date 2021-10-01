ad9d7767d7e3a0726c0c280d8e10e0b8fb7d8e12
Rafael Maia <rafaelmaiadata@gmail.com>
2021-09-30 23:36:31 -0300

    Exception handling - delete

---
+++ src/main/java/io/github/rfmaia/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package io.github.rfmaia.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
